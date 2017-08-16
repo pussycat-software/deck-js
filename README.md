1a839e3181d6adb8507723fe9fecc3159d1066b5
Kirill K <kovalev.kirill.a@gmail.com>
2017-08-16 03:23:47 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Description
+
+HTML + JavaScript page to ease dealing virtual deck of cards.
+
+# Usage
+
+Edit `./js/script.js`:
+
+- Provide address of working [Cards API](https://github.com/pussycat-software/cards)
+- Define deck type, `russian` or `french`
+
+Deploy somewhere or just open `index.html` locally.
+
+# Resources used
+
+Code:
+
+- https://getbootstrap.com/
+- https://jquery.com/
+
+Design:
+
+- https://designshack.net/articles/css/css-card-tricks/
+- https://habrahabr.ru/post/315216/
---
+++ js/script.js
@@ -0,0 +1,90 @@
+var dealer_api = 'https://cards.agrrh.com/api/v1/';
+var deck_type = 'ru';
+
+var deck_id = false;
+
+function card_decode(card) {
+  value = card[0].toUpperCase();
+  // 0 to 10 workaround
+  if (value == '0') {
+    value = '10';
+  }
+
+  suits = {
+    'h': 'hearts',
+    'd': 'diamonds',
+    'c': 'clubs',
+    's': 'spades'
+  }
+  suit = suits[card[1]];
+
+  return [value, suit];
+}
+
+function deck_new() {
+  $.get( dealer_api + 'deal/new/' + deck_type, function( data ) {
+    console.log(data);
+    deck_id = data.id;
+
+    card = data.card;
+    left = data.left;
+
+    draw_history(false);
+    draw_actual(card);
+    draw_left(left);
+
+    return deck_id;
+  });
+}
+
+function deck_resume(id) {
+  $.get( dealer_api + 'deal/' + id, function( data ) {
+    console.log(data);
+
+    old_card = card;
+
+    card = data.card;
+    left = data.left;
+
+    draw_history(old_card);
+    draw_actual(card);
+    draw_left(left);
+  });
+}
+
+function draw_actual(card) {
+  if (card != null) {
+    [value, suit] = card_decode(card);
+
+    $( ".hand .actual .col" ).html(
+      '<div class="card face suit-' + suit + '">' + value + '</div>'
+    );
+  } else {
+    $( ".hand .actual .col" ).html('<div class="card"></div>');
+    deck_id = false;
+  }
+}
+
+function draw_left(count) {
+  $( ".deck .card" ).text(count);
+}
+
+function draw_history(card) {
+  if (card) {
+    [value, suit] = card_decode(card);
+
+    $( ".hand .history .col" ).prepend(
+      '<div class="card face suit-' + suit + '">' + value + '</div>'
+    );
+  } else {
+    $( ".hand .history .col" ).html('');
+  }
+}
+
+$( ".deck .card" ).click(function() {
+  if (deck_id) {
+    deck_resume(deck_id);
+  } else {
+    deck_id = deck_new();
+  }
+});
